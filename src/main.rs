5eab2a9cb4fae24ab9d0d8d7449cbff8077caf57
Guy Morand <guy.morand@bytesatwork.ch>
2024-04-09 15:49:58 +0200

    Advanced hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn say_hello(who: &String) {
+    println!("Hello {}", who);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut who: String = String::from("World");
+    say_hello(&who);
+    who = String::from("bytesatwork");
+    say_hello(&who);
 }

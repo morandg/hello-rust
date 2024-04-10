1c21a5fdb7d49e0fc9677b5b1684bd95045010ed
Guy Morand <guy.morand@bytesatwork.ch>
2024-04-10 10:32:08 +0200

    Argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod fibonacci;
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct UserArgs {
+    fibonacci_number: u32,
+}
+
 fn main() {
-    let n = 5;
+    let user_args = UserArgs::parse();
+    let n = user_args.fibonacci_number;
     println!("Fibonacci of {} is {}", n, fibonacci::fibonacci(n));
 }

a7d73cff2fa31b015d58b470d28d79ecbc21c756
Guy Morand <guy.morand@bytesatwork.ch>
2024-04-10 15:39:05 +0200

    Check vector out of bound

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -10,18 +10,21 @@ impl FibonacciSolutions {
   }
 
   pub fn find_solution(&mut self, n: u128) -> Result<u128, String> {
-    let solutions_idx = usize::try_from(n).unwrap();
+    match usize::try_from(n) {
+      Err(error) => Err(format!("Could not get index of the solution vector: {}", error)),
+      Ok(solutions_idx) => self.compute_solution(solutions_idx, n),
+    }
+  }
 
+  fn compute_solution(&mut self, solutions_idx: usize, n: u128) -> Result<u128, String> {
     if solutions_idx + 1 > self.solutions.len() {
       let fibo1 = self.find_solution(n - 1)?;
       let fibo2 = self.find_solution(n - 2)?;
-
       match fibo1.checked_add(fibo2) {
           Some(new_solution) => self.solutions.push(new_solution),
           None => return Err(format!("Fibo of {} overflow!", n)),
       }
     }
-
     Ok(self.solutions[solutions_idx])
   }
 }
@@ -51,5 +54,11 @@ mod tests {
   fn test_fibo_of_big_number_overflows() {
     let mut fibo_solutions = FibonacciSolutions::new();
     assert!(matches!(fibo_solutions.find_solution(200), Err(_)));
+  }
+
+  #[test]
+  fn test_fibo_of_big_number_overflows_vector_size() {
+    let mut fibo_solutions = FibonacciSolutions::new();
+    assert!(matches!(fibo_solutions.find_solution(u128::MAX), Err(_)));
   }
 }
